bb9a59bec5e8853ab7bc9e526b9f206fdc8d5885
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-01-22 11:31:34 -0800

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardRecipients = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cardRecipients) {
+  let i = 0; 
+  let cardMessage = []
+  while (i < cardRecipients.length) {
+    cardMessage.push(`Thank you, ${cardRecipients[i]}, for the wonderful surprise gift!`);
+    i++;
+  }
+return cardMessage;
+}
+
+function countDown(){
+  for (let i= 10; i >= 0; i--) {
+    console.log(i);
+  }
+}
